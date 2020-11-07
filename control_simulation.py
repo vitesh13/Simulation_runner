509632dbf3d96ce315436faae564a99e2e7be8ae
Vitesh Shah <v.shah@mpie.de>
2020-11-07 13:45:27 +0100

    Can pause simulation, access hdf5 continue simulation and send signal

--- control_simulation.py
+++ control_simulation.py
@@ -8,46 +8,18 @@ import subprocess, signal, time, os
 import damask
 import h5py
 
-#cmd = "DAMASK_spectral -l tensionX.load -g 20grains16x16x16.geom > check.txt"
-#
-#P = subprocess.Popen(cmd,shell=True)
-#
-#while P.poll() != 0:
-#   time.sleep(5)
-#   os.kill(P.pid+1, signal.SIGSTOP)
-#   print("doing something")
-#   d = damask.Result('20grains16x16x16_tensionX.hdf5')
-#   path = d.get_dataset_location('F')[-1]
-#   f = h5py.File('20grains16x16x16_tensionX.hdf5')
-#   print('values',f[path][0,:,:])
-#   os.kill(P.pid, signal.SIGCONT)
-
-
 cmd = "DAMASK_grid -l tensionX.yaml -g 20grains16x16x16.vtr"
-#f = open('check.txt','w')
-
 with open('check.txt','wb') as f:
-  P = subprocess.Popen(cmd,stdout = subprocess.PIPE, stderr = subprocess.PIPE,shell=True)
-
-  while P.poll() is None:
+    P = subprocess.Popen(cmd,stdout = subprocess.PIPE, stderr = subprocess.PIPE,shell=True)
     r = re.compile(' increment 3 converged')
-    for line in iter(P.stdout.readline, b''):
-      if re.search(r,P.stdout.readline().decode('utf-8')):
-        print("hello")
-        os.kill(P.pid+1,signal.SIGUSR1)
-      f.write(line)
-
-
-
-
-#while P.poll() != 0:
-#   time.sleep(1)
-#   os.kill(P.pid+1, signal.SIGSTOP)
-#   print("doing something")
-#   d = damask.Result('20grains16x16x16_tensionX.hdf5')
-#   path = d.get_dataset_location('F')
-#   print(path)
-#   #f = h5py.File('20grains16x16x16_tensionX.hdf5')
-#   #print('values',f[path][0,:,:])
-#   #os.kill(P.pid, signal.SIGCONT)
-
+    record = []
+    while P.poll() is None:
+      for count,line in enumerate(iter(P.stdout.readline, b'')):
+             record.append(line.decode('utf-8'))
+             if count > 1 and (count%300) == 0:
+               os.kill(P.pid+1, signal.SIGSTOP)
+               d = damask.Result('20grains16x16x16_tensionX.hdf5')
+               paths = d.get_dataset_location('F')
+               os.kill(P.pid+1, signal.SIGCONT)
+               if len(paths) == 5:
+                 os.kill(P.pid+1, signal.SIGUSR1)
