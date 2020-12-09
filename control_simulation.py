e07af7736334ca4dd6f5502d7d0e98bca1152513
Vitesh Shah <v.shah@mpie.de>
2020-12-09 16:31:16 +0100

    recording to file and sending signals

--- control_simulation.py
+++ control_simulation.py
@@ -7,19 +7,22 @@
 import subprocess, signal, time, os
 import damask
 import h5py
+import re
 
 cmd = "DAMASK_grid -l tensionX.yaml -g 20grains16x16x16.vtr"
-with open('check.txt','wb') as f:
+with open('check.txt','w') as f:
     P = subprocess.Popen(cmd,stdout = subprocess.PIPE, stderr = subprocess.PIPE,shell=True)
     r = re.compile(' increment 3 converged')
     record = []
     while P.poll() is None:
       for count,line in enumerate(iter(P.stdout.readline, b'')):
              record.append(line.decode('utf-8'))
-             if count > 1 and (count%300) == 0:
+             if re.search(r, record[-1]):
                os.kill(P.pid+1, signal.SIGSTOP)
                d = damask.Result('20grains16x16x16_tensionX.hdf5')
-               paths = d.get_dataset_location('F')
+               print(d.get_dataset_location('F'))
                os.kill(P.pid+1, signal.SIGCONT)
-               if len(paths) == 5:
-                 os.kill(P.pid+1, signal.SIGUSR1)
+    for line in record:
+      f.write(line)
+
+
