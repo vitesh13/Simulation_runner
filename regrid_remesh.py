48acd39b9e46e713f2adcf494c073357e9719e2f
Vitesh Shah <v.shah@mpie.de>
2021-03-26 10:14:32 +0100

    creates regridded text file now

--- regrid_remesh.py
+++ regrid_remesh.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 no BOM -*-
 
-import os,sys,math,re,time,struct
+import os,math,re,struct
 import h5py
 import re
 import numpy as np
-import os, sys, shutil
+import os, shutil
 import scipy
 from scipy import spatial
 from scipy.interpolate import griddata
-import argparse
-import subprocess
 import damask
 import pandas as pd
 import itertools as it
@@ -20,9 +18,12 @@ from numpy.linalg import inv
 import shutil
 from imp import reload
 
-from damask import regriding as rgg
+#from damask import regriding as rgg
+from damask import geom_regridder
+from damask import restart_regridder
 from damask import Rotation
-from output_reader import output_reader
+from damask import Orientation
+#from output_reader import output_reader
 
 # combining the codes from 0_main_all and remesh_coords
 
@@ -54,6 +55,8 @@ class Remesh_for_CA():
   def main_all(self,geom,load,inc,folder):
     """
     Regrids the data and generates a HDF5 and text file for CA.
+    Generates a new restart file using regridding framework.
+    Also create a new hdf5 results file for deformed microstructure. 
     
     Parameters
     ----------
@@ -70,23 +73,17 @@ class Remesh_for_CA():
     isElastic = False
     scale = 1.0
     grid = False 
-    os.chdir('/nethome/v.shah/{}/'.format(folder))
-    geom_name = rgg.setting_util.remove_fileFormat(geom)
-    load_name = rgg.setting_util.remove_fileFormat(load)
-    isElastic = rgg.setting_util.set_elasticDeformation(isElastic)
-    seedScale = rgg.setting_util.set_scale4gridSeedsRegridding(scale, grid)
+    os.chdir(folder)
 
-    hdf5_name = '%s_%s.hdf5'%(geom_name,load_name)
-    history_name = '%s_%s_regriddingHistory.hdf5'%(geom_name,load_name)
-    
-    rg = rgg.geom_regridder(geom_name,load_name,increment=inc)
-    rg.read_geom()
-    print(rg.microstructureFlatten_0.shape)
-    rg.read_h5Output(inc)   #this function also gets the displacements of nodes and cells apparently
+    rg = geom_regridder(geom,load,increment=inc)
     rg.regrid_geom()
+    rg.write_geomRegridded()
+  
+    rgr = restart_regridder(geom,load,increment_title='inc{}'.format(inc))
+    rgr.write_h5OutRestart(rg.nearestNeighbors_locs,rg.gridSeeds_regrid,isElastic)
+    
     self.new_size = rg.sizeRVE_regrid
     self.new_grid = rg.gridSeeds_regrid
-    print('new size type',type(self.new_size))
    
     # Building the new coordinates
     elem0 = int(rg.gridSeeds_0.prod())
@@ -98,39 +95,32 @@ class Remesh_for_CA():
     Cell_coords = rg.gridCoords_cell_regrid   #or it should be after the periodic shift??
     print(Cell_coords.shape)
 
-    #------------------------------------------
-    # reading main inputs for processing
-    out5 = output_reader(hdf5_name)
-    inc, inc_key = out5.make_incerement(inc)
-    phase_name = out5.constituents[0]
-    # reading main inputs
-    orientations = np.array(out5.data[inc_key]['constituent'][phase_name]['generic']['orientation'])
-    orientations = np.array(orientations.tolist())
-    ## grain rotation
-    grain_rotation = np.array(out5.data[inc_key]['constituent'][phase_name]['generic']['grain_rotation'])  #need to add [:,3] is using damask generated output
+    ##------------------------------------------
+    ## reading main inputs for processing
+    #out5 = output_reader(hdf5_name)
+    #inc, inc_key = out5.make_incerement(inc)
+    ## reading main inputs
+    d = damask.Result(rg.h5OutputName_0 + '.hdf5')
+    phase_name = d.phases 
+    d.view('increments',f'inc{inc}')
+    d.get_dataset_location('O')
+    orientations = d.read_dataset(d.get_dataset_location('O'))
+    ### grain rotation
+    grain_rotation = d.read_dataset(d.get_dataset_location('reorientation'))
     grain_rotation_rg = grain_rotation[rg.get_nearestNeighbors()]
     grain_rotation_rg_scalar = grain_rotation_rg
     mylist = []
-    for count,ori in enumerate(orientations):
-      o = Rotation.fromQuaternion(list(ori))
-      mylist.append(o.asEulers(degrees=None).tolist())
-    
-    eulers = np.array(mylist)
+    eulers = Orientation(orientations).as_Euler_angles(degrees=True)
     eulers_rg = eulers[rg.get_nearestNeighbors()]
-    ## dislocation density
-    rho_m = np.array(out5.data[inc_key]['constituent'][phase_name]['plastic']['rho_mob'])
-    rho_d = np.array(out5.data[inc_key]['constituent'][phase_name]['plastic']['rho_dip'])
-    rho = rho_m + rho_d
-    ## reshaped
-    rho_m_rg = rho_m[rg.get_nearestNeighbors()]
-    rho_d_rg = rho_d[rg.get_nearestNeighbors()]
+    ### dislocation density
+    rho =  d.read_dataset(d.get_dataset_location('tot_density'))
+    ### reshaped
     rho_rg = rho[rg.get_nearestNeighbors()]
-    rho= np.sum(rho_rg,axis=1)
-    ## subgrain sizes
-    r_s   = np.array(out5.data[inc_key]['constituent'][phase_name]['plastic']['r_s']) 
+    ### subgrain sizes
+    r_s    =  d.read_dataset(d.get_dataset_location('r_s'))
     r_s_rg   = r_s[rg.get_nearestNeighbors()]
-    #
-    print('orientation shape before: ',np.shape(orientations))
+    #print('orientation shape before: ',np.shape(orientations))
+    # orientations regridded
     orientations_rg = orientations[rg.get_nearestNeighbors()] #hopefully it works
     print('orientation shape after: ',np.shape(orientations_rg))
 
@@ -141,17 +131,17 @@ class Remesh_for_CA():
     
     df = pd.DataFrame() 
     # coords for new grids
-    Cell_coords = Cell_coords*1E-06
+    Cell_coords = Cell_coords#*1E-06
+    print(Cell_coords)
     df['x'] = Cell_coords[:,0]
     df['y'] = Cell_coords[:,1]
     df['z'] = Cell_coords[:,2]
-    # initial grain
-    #df['grain'] = np.array(df_cell['grain'])[rebacked_id]
-    df['grain'] = rg.microstructureFlatten_0[rg.nearestNeighbors_locs] 
+    ## initial grain
+    df['grain'] = rg.materialFlatten_0[rg.nearestNeighbors_locs] #casipt starts counting from 1 I guess 
     ## Rotation
     df['Rotation'] = grain_rotation_rg_scalar
     # # total dislo
-    df['rho'] = rho
+    df['rho'] = rho_rg
     # # subgrain sizes 
     df['r_s'] = r_s_rg
     # euler angles
@@ -161,8 +151,8 @@ class Remesh_for_CA():
 
     header_f = '%s %s\n'%(str(int(np.prod(rg.gridSeeds_regrid))),str(max(df['grain']))) #,str(max(df['grain']))
     
-    output = '%s_%s'%(geom_name,load_name)
-    file_rg = '%s_%s.txt'%(output,inc)
+    output = rg.h5OutputName_0 
+    file_rg = '%s_inc%s.txt'%(output,inc)
     if not os.path.exists(os.path.join('/nethome/v.shah',folder,'postProc')):
         os.makedirs(os.path.join('/nethome/v.shah',folder,'postProc'))
     
@@ -174,57 +164,52 @@ class Remesh_for_CA():
                                                 "{:.8f}".format],index=False)
     
     #create a hdf5 file
-    new_hdf_name = 'new_' + geom_name + '_' + load_name + '_' + str(inc) + '.hdf5'
-    hdf = h5py.File(new_hdf_name,'w')
-    hdf.attrs['DADF5_version_major'] = 0
-    hdf.attrs['DADF5_version_minor'] = 6
-    hdf.attrs['DADF5-version'] = 0.2
-    hdf.create_group('geometry')
-    hdf['geometry'].attrs['grid'] = np.array(rg.gridSeeds_regrid, np.int32)
-    hdf['geometry'].attrs['size'] = np.array(rg.sizeRVE_regrid, np.float64)
-    hdf['geometry'].attrs['origin'] = np.array(rg.originRVE_0,np.float64)
+    #new_hdf_name = 'new_' + rg.h5OutputName_0 + 'inc' + str(inc) + '.hdf5'
+    #hdf = h5py.File(new_hdf_name,'w')
+    #hdf.attrs['DADF5_version_major'] = 0
+    #hdf.attrs['DADF5_version_minor'] = 11
+    #hdf.attrs['DAMASK_version'] = 'v3.0.0-alpha2-619-ga99983145'
+    #hdf.create_group('geometry')
+    #hdf['geometry'].attrs['cells'] = np.array(rg.gridSeeds_regrid, np.int32)
+    #hdf['geometry'].attrs['size'] = np.array(rg.sizeRVE_regrid, np.float64)
+    #hdf['geometry'].attrs['origin'] = np.array(rg.originRVE_0,np.float64)
+    ##
+    ###mapping data
+    #comp_dtype = np.dtype([('Name',np.string_,64),('Position',np.int32)])
+    #new_len    = np.prod(np.int32(rg.gridSeeds_regrid))
+    #data_name  = phase_name*int(new_len)
+    #data_value = [i for i in range(new_len)]
+    #new_data   = list(zip(data_name,data_value))
+    #print(new_data)
+    #new_data   = np.array(new_data,dtype=comp_dtype)
+    #new_data   = new_data.reshape(new_len,1)
+    #dataset    = hdf.create_dataset("/mapping/phase",(new_len,1),comp_dtype)
+    #dataset[...] = new_data
+
+    ###orientation_rg
+    ##comp_dtype  = np.dtype([('w',np.float64),('x',np.float64),('y',np.float64),('z',np.float64)])
+    ###new_len     = np.prod(np.int32(rg.new_grid))
+    ##new_len     = np.prod(np.int32(rg.gridSeeds_regrid))
+    #dataset_ori = hdf.create_dataset("/inc{}/phase/{}/mechanics/O".format(inc,phase_name[0]),(new_len,4)) #will work for single phase only
+    ##orientations_rg = np.array([tuple(i) for i in orientations_rg[:]],dtype=comp_dtype)
+    #dataset_ori[...] = orientations_rg
+    #hdf["/inc{}/phase/{}/mechanics/O".format(inc,phase_name[0])].attrs['Lattice'] = 'cF'
+    #hdf["/inc{}/phase/{}/mechanics/O".format(inc,phase_name[0])].attrs['Unit'] = 'q_0 (q_1 q_2 q_3)'
+    ##
+    ###rho_rg and grain_rotation_rg_scalar
+    #dataset_rho = hdf.create_dataset("/inc{}/phase/{}/plastic/tot_density".format(inc,phase_name[0]),(new_len,))
+    #dataset_rho[...] = rho
     #
-    ##mapping data
-    comp_dtype = np.dtype([('Name',np.string_,64),('Position',np.int32)])
-    #new_len    = np.prod(np.int32(rg.new_grid))
-    new_len    = np.prod(np.int32(rg.gridSeeds_regrid))
-    data_name  = [phase_name]*int(new_len)
-    data_value = [i for i in range(new_len)]
-    new_data   = list(zip(data_name,data_value))
-    new_data   = np.array(new_data,dtype=comp_dtype)
-    new_data   = new_data.reshape(new_len,1)
-    dataset    = hdf.create_dataset("/mapping/cellResults/constituent",(new_len,1),comp_dtype)
-    dataset[...] = new_data
+    #dataset_rot = hdf.create_dataset("/inc{}/phase/{}/plastic/reorientation".format(inc,phase_name[0]),(new_len,))
+    #dataset_rot[...] = grain_rotation_rg_scalar
+    ##
+    #hdf.create_group('/inc{}/homogenization/{}/damage'.format(inc,d.homogenizations[0]))  #works for single homogenization currently
+    #hdf.create_group('/inc{}/homogenization/{}/mech'.format(inc,d.homogenizations[0]))  #works for single homogenization currently
+    #hdf.create_group('/inc{}/homogenization/{}/thermal'.format(inc,d.homogenizations[0]))  #works for single homogenization currently
+    ##
+    #hdf.create_dataset('/inc{}/geometry/u_n'.format(inc),data=np.zeros(new_len,3))
+    #hdf.create_dataset('/inc{}/geometry/u_p'.format(inc),data=np.zeros(new_len,3))
     
-    data_name  = ['1_SX']*int(new_len)
-    new_data   = list(zip(data_name,data_value))
-    new_data   = np.array(new_data,dtype=comp_dtype)
-    new_data   = new_data.reshape(new_len,1)
-    dataset    = hdf.create_dataset("/mapping/cellResults/materialpoint",(new_len,1),comp_dtype)
-    dataset[...] = new_data
-    
-    #orientation_rg
-    comp_dtype  = np.dtype([('w',np.float64),('x',np.float64),('y',np.float64),('z',np.float64)])
-    #new_len     = np.prod(np.int32(rg.new_grid))
-    new_len     = np.prod(np.int32(rg.gridSeeds_regrid))
-    dataset_ori = hdf.create_dataset("/{}/constituent/{}/generic/orientation".format(inc_key,phase_name),(new_len,),comp_dtype)
-    orientations_rg = np.array([tuple(i) for i in orientations_rg[:]],dtype=comp_dtype)
-    dataset_ori[...] = orientations_rg
-    hdf["/{}/constituent/{}/generic/orientation".format(inc_key,phase_name)].attrs['Lattice'] = 'bcc'
-    
-    #rho_rg and grain_rotation_rg_scalar
-    dataset_rho = hdf.create_dataset("/{}/constituent/{}/plastic/tot_density".format(inc_key,phase_name),(new_len,))
-    dataset_rho[...] = rho
-    
-    dataset_rot = hdf.create_dataset("/{}/constituent/{}/generic/grain_rotation".format(inc_key,phase_name),(new_len,))
-    dataset_rot[...] = grain_rotation_rg_scalar
-    
-    hdf.create_group('/{}/materialpoint/1_SX/generic'.format(inc_key))
-    hdf.create_group('/{}/materialpoint/1_SX/plastic'.format(inc_key))
-    
-    
-    #hdf.create_group('inc{}'.format(inc))
-    print(inc_key)
     return self.new_size,self.new_grid
 
   def remesh_coords(self,filename,unit,folder):
@@ -240,8 +225,8 @@ class Remesh_for_CA():
     folder : str
       simulation folder
     """ 
-    os.chdir('/nethome/v.shah/{}/postProc'.format(folder))
-    dx = np.min(self.new_size/self.new_grid)*1E-06
+    os.chdir('{}'.format(folder))
+    dx = np.min(self.new_size/self.new_grid)#*1E-06
     print(dx)
     unit_scale = unit
     
