d4deb1ada8708668136f819a0598bfe224676500
Vitesh Shah <v.shah@mpie.de>
2021-03-30 12:24:30 +0200

    findNeighbours adopted to new restart file style

--- After_CA_restart.py
+++ After_CA_restart.py
@@ -5,7 +5,20 @@ import numpy as np
 import os
 from scipy import spatial
 import h5py
+from damask import Grid
+from damask import ConfigMaterial as cm
+from damask import Rotation
+from damask import Orientation
 from Fe_decomposition import Decompose
+
+#--------------------------------------------------------------------------
+Crystal_structures = {'fcc': 1,
+                      'bcc': 1,
+                      'hcp': 0,
+                      'bct': 7,
+                      'ort': 6} #TODO: is bct Tetragonal low/Tetragonal high?
+Phase_types = {'Primary': 0} #further additions to these can be done by looking at 'Create Ensemble Info' filter
+#--------------------------------------------------------------------------
 
 def eulers_toR(eulers):
   """
@@ -96,6 +109,31 @@ def qu2om(qu):
 
 # --------------------------------------------------------------------
 
+class AttributeManagerNullterm(h5py.AttributeManager): 
+  """
+  Attribute management for DREAM.3D hdf5 files.
+  
+  String attribute values are stored as fixed-length string with NULLTERM
+  
+  References
+  ----------
+    https://stackoverflow.com/questions/38267076
+    https://stackoverflow.com/questions/52750232
+
+  """ 
+
+  def create(self, name, data, shape=None, dtype=None):
+    if isinstance(data,str):
+      tid = h5py.h5t.C_S1.copy()
+      tid.set_size(len(data + ' '))
+      super().create(name=name,data=data+' ',dtype = h5py.Datatype(tid))
+    else:
+      super().create(name=name,data=data,shape=shape,dtype=dtype)
+     
+
+h5py._hl.attrs.AttributeManager = AttributeManagerNullterm # 'Monkey patch'
+
+
 class CASIPT_postprocessing():
   """
   Class combining different scripts for CASIPT post-processing.
@@ -114,7 +152,7 @@ class CASIPT_postprocessing():
     
     #self.simulation_folder = 'DRX_sample_simulations'
     self.CA_folder         = CA_folder 
-    self.CA_geom           = 'resMDRX.3D.geom'
+    self.CA_geom           = '.3D.geom'
 
   
   def geomFromCA(self,dx):
@@ -131,50 +169,163 @@ class CASIPT_postprocessing():
     with open(self.CA_geom,'r') as f:
       grid_size = f.readline().split()[2:7:2] #read only first line
 
+    grain_id = np.loadtxt(self.CA_geom,skiprows=1,usecols=(1))
     grid_size = np.array([int(i) for i in grid_size])
  
     n_elem = np.prod(grid_size)   #need to check if it is correct for 2D geometry
 
-    data_to_geom = []
-    data_to_geom.append('{} header'.format(1))  #so that we can modify this later
-    data_to_geom.append('grid a {} b {} c {}'.format(*grid_size))
-    data_to_geom.append('size x {} y {} z {}'.format(*(grid_size*dx))) #give input that will kind of decide the spacing (like in DREAM3D)
-    data_to_geom.append('origin x 0.0 y 0.0 z 0.0')
-    data_to_geom.append('homogenization 1')
-    
-    geom_data_1 = np.loadtxt('resMDRX.texture_MDRX.txt',skiprows=0,usecols=(2))
-    print(np.shape(geom_data_1),geom_data_1)
-    geom_data_1 = geom_data_1.tolist()
-    data_to_geom.append('microstructures {}'.format(int(np.amax(geom_data_1))+1))
+    size = grid_size*dx
+    return Grid(grain_id.reshape(grid_size,order='F'),size)
 
-    #write microstructure part
-    data_to_geom.append('<microstructure>')
+    #data_to_geom = []
+    #data_to_geom.append('{} header'.format(1))  #so that we can modify this later
+    #data_to_geom.append('grid a {} b {} c {}'.format(*grid_size))
+    #data_to_geom.append('size x {} y {} z {}'.format(*(grid_size*dx))) #give input that will kind of decide the spacing (like in DREAM3D)
+    #data_to_geom.append('origin x 0.0 y 0.0 z 0.0')
+    #data_to_geom.append('homogenization 1')
     
-    for count,grain in enumerate(geom_data_1):
-      data_to_geom.append('[Grain{:02d}]'.format(int(grain)+1))
-      data_to_geom.append('crystallite 1')
-      data_to_geom.append('(constituent) phase 1 texture {} fraction 1.0'.format(int(grain)+1))
-    
-    #write texture part
-    data_to_geom.append('<texture>')
-    texture_data = np.loadtxt('resMDRX.texture_MDRX.txt',skiprows=0,usecols=((4,6,8)))
-    
-    for i,texture in enumerate(texture_data):
-      data_to_geom.append('[Grain{:02d}]'.format(i+1))
-      data_to_geom.append('(gauss) phi1 {} Phi {} phi2 {} scatter 0.0 fraction 1.0'.format(*texture))
+    #geom_data_1 = np.loadtxt('resMDRX.texture_MDRX.txt',skiprows=0,usecols=(2))
+    #print(np.shape(geom_data_1),geom_data_1)
+    #geom_data_1 = geom_data_1.tolist()
+    #data_to_geom.append('microstructures {}'.format(int(np.amax(geom_data_1))+1))
 
-    # calculating header length
-    header_value = len(data_to_geom) - 1
-    data_to_geom[0] = '{} header'.format(header_value)
-    geom_data = np.loadtxt('resMDRX.3D.geom',skiprows=1,usecols=(1))
-    numbers = geom_data.astype(int) + 1
-    numbers = numbers.reshape([grid_size[0],np.prod(grid_size[1:])],order='F').T
-    
-    for line in data_to_geom:
-      print(line)
-    
-    np.savetxt('test.geom',numbers,fmt='%s',newline='\n',header='\n'.join(data_to_geom),comments='') 
+    ##write microstructure part
+    #data_to_geom.append('<microstructure>')
+    #
+    #for count,grain in enumerate(geom_data_1):
+    #  data_to_geom.append('[Grain{:02d}]'.format(int(grain)+1))
+    #  data_to_geom.append('crystallite 1')
+    #  data_to_geom.append('(constituent) phase 1 texture {} fraction 1.0'.format(int(grain)+1))
+    #
+    ##write texture part
+    #data_to_geom.append('<texture>')
+    #texture_data = np.loadtxt('resMDRX.texture_MDRX.txt',skiprows=0,usecols=((4,6,8)))
+    #
+    #for i,texture in enumerate(texture_data):
+    #  data_to_geom.append('[Grain{:02d}]'.format(i+1))
+    #  data_to_geom.append('(gauss) phi1 {} Phi {} phi2 {} scatter 0.0 fraction 1.0'.format(*texture))
 
+    ## calculating header length
+    #header_value = len(data_to_geom) - 1
+    #data_to_geom[0] = '{} header'.format(header_value)
+    #geom_data = np.loadtxt('resMDRX.3D.geom',skiprows=1,usecols=(1))
+    #numbers = geom_data.astype(int) + 1
+    #numbers = numbers.reshape([grid_size[0],np.prod(grid_size[1:])],order='F').T
+    #
+    #for line in data_to_geom:
+    #  print(line)
+    
+
+  def CAtoDREAM3D(self,dx):
+    """
+    Creates a dream3D file from CA output.
+    This Dream3D file can then be used by DAMASK library to create the new geom and material.yaml.
+
+    Parameters:
+    -----------
+    dx : float
+      The grid spacing.
+    """
+    #--------------------------------------------------------------------------
+    #Build array of euler angles for each cell
+    #--------------------------------------------------------------------------
+    cell_orientation_array  = np.loadtxt('..ang',skiprows=0,usecols=(0,1,2)) # ID phi1 phi phi2
+    grain_orientation_array = np.loadtxt('.texture_MDRX.txt',skiprows=0,usecols=(4,6,8)) # ID phi1 phi phi2 
+    
+    with open(self.CA_geom,'r') as f:
+      grid_size = f.readline().split()[2:7:2] #read only first line
+
+    grid_size = np.array(grid_size)
+
+    #--------------------------------------------------------------------------
+    o = h5py.File('CA_output.dream3D','w')
+    o.attrs['DADF5toDREAM3D'] = '1.0'
+    o.attrs['FileVersion']    = '7.0'
+
+    for g in ['DataContainerBundles','Pipeline']: # empty groups (needed)
+      o.create_group(g)
+
+    data_container_label = 'DataContainers/SyntheticVolumeDataContainer'        
+    cell_data_label      = data_container_label + '/CellData'
+
+    # Data phases
+    o[cell_data_label + '/Phases'] = np.ones(tuple(np.flip(grid_size))+(1,),dtype=np.int32)
+
+    # Data eulers
+    orientation_data = cell_orientation_array.astype(np.float32)
+    o[cell_data_label + '/Eulers'] = orientation_data.reshape(tuple(np.flip(grid_size))+(3,))
+
+    # Attributes to CellData group
+    o[cell_data_label].attrs['AttributeMatrixType'] = np.array([3],np.uint32)
+    o[cell_data_label].attrs['TupleDimensions']     = np.array(grid_size,np.uint64)
+
+    # Common Attributes for groups in CellData
+    for group in ['/Phases','/Eulers']:
+      o[cell_data_label + group].attrs['DataArrayVersion']      = np.array([2],np.int32)
+      o[cell_data_label + group].attrs['Tuple Axis Dimensions'] = 'x={},y={},z={}'.format(*np.array(grid_size))
+    
+    # phase attributes
+    o[cell_data_label + '/Phases'].attrs['ComponentDimensions'] = np.array([1],np.uint64)
+    o[cell_data_label + '/Phases'].attrs['ObjectType']          = 'DataArray<int32_t>'
+    o[cell_data_label + '/Phases'].attrs['TupleDimensions']     = np.array(grid_size,np.uint64)
+    
+    # Eulers attributes
+    o[cell_data_label + '/Eulers'].attrs['ComponentDimensions'] = np.array([3],np.uint64)
+    o[cell_data_label + '/Eulers'].attrs['ObjectType']          = 'DataArray<float>'        
+    o[cell_data_label + '/Eulers'].attrs['TupleDimensions']     = np.array(grid_size,np.uint64)
+
+    # Create EnsembleAttributeMatrix
+    ensemble_label = data_container_label + '/CellEnsembleData'
+
+    # Attributes Ensemble Matrix
+    o[ensemble_label].attrs['AttributeMatrixType'] = np.array([11],np.uint32)
+    o[ensemble_label].attrs['TupleDimensions']     = np.array([2], np.uint64)
+
+    # Data CrystalStructures
+    o[ensemble_label + '/CrystalStructures'] = np.uint32(np.array([999,1]))
+    #                                                Crystal_structures[f.get_crystal_structure()]])).reshape((2,1))
+    o[ensemble_label + '/PhaseTypes']        = np.uint32(np.array([999,Phase_types['Primary']])).reshape((2,1))
+
+    # Attributes for data in Ensemble matrix
+    for group in ['CrystalStructures','PhaseTypes']: # 'PhaseName' not required MD: But would be nice to take the phase name mapping
+      o[ensemble_label+'/'+group].attrs['ComponentDimensions']   = np.array([1],np.uint64)
+      o[ensemble_label+'/'+group].attrs['Tuple Axis Dimensions'] = 'x=2'
+      o[ensemble_label+'/'+group].attrs['DataArrayVersion']      = np.array([2],np.int32)
+      o[ensemble_label+'/'+group].attrs['ObjectType']            = 'DataArray<uint32_t>'
+      o[ensemble_label+'/'+group].attrs['TupleDimensions']       = np.array([2],np.uint64)
+
+    # Create geometry info
+    geom_label = data_container_label + '/_SIMPL_GEOMETRY'
+    
+    o[geom_label + '/DIMENSIONS'] = np.int64(np.array(grid_size))
+    o[geom_label + '/ORIGIN']     = np.float32(np.zeros(3))
+    #o[geom_label + '/SPACING']    = np.float32(np.array(dummy)*4)
+    o[geom_label + '/SPACING']    = np.float32(np.ones(3)*dx*1E6)
+        
+    o[geom_label].attrs['GeometryName']     = 'ImageGeometry'
+    o[geom_label].attrs['GeometryTypeName'] = 'ImageGeometry'
+    o[geom_label].attrs['GeometryType']          = np.array([0],np.uint32) 
+    o[geom_label].attrs['SpatialDimensionality'] = np.array([3],np.uint32) 
+    o[geom_label].attrs['UnitDimensionality']    = np.array([3],np.uint32) 
+
+  def config_from_CA(self,simulation_folder):
+    """
+    Creates a material.yaml file
+
+    Parameters
+    ----------
+    simulation_folder : str
+      Path of the simulation_folder
+    """
+    ori = np.loadtxt('.texture_MDRX.txt',usecols=(4,6,8))
+    ori = Rotation.from_Euler_angles(ori).as_quaternion().reshape(-1,4)
+    base_config = cm.load('{}/material.yaml'.format(simulation_folder))
+    phase = [base_config['material'][0]['constituent']['phase']]*len(ori)
+    idx = np.arange(len(ori))
+    constituent = {k:np.atleast_1d(v[idx].squeeze()) for k,v in zip(['O','phase'],[O,phase])}
+    new_config = base_config.material_add(**constituent,homogenization='direct')
+    new_config.save()
+ 
   def findNeighbours(self,regrid,remesh,hdf):
     """
     Finds neighbours by comparing the original damask datapoint and remeshed datapoints.
@@ -190,6 +341,7 @@ class CASIPT_postprocessing():
       Path of the regridded hdf5 from Karos code. 
 
     """
+    os.chdir(os.path.dirname(hdf))
     regridding_coords = np.loadtxt(regrid,skiprows=1,usecols=(0,1,2))
     remeshed_coords   = np.loadtxt(remesh,skiprows=1,usecols=(0,1,2))
     print('length',len(remeshed_coords))
@@ -206,42 +358,49 @@ class CASIPT_postprocessing():
     
     hdf = h5py.File(hdf)
 
-    const_values = ['C_minMaxAvg','C_volAvg','C_volAvgLastInc','F_aim','F_aimDot','F_aim_lastInc']
+    names_in_big_groups_dict = {}
+    big_groups = ['homogenization','phase']
+    for i in big_groups:
+      names_in_big_groups_dict[i] = hdf[i].keys()
+
+    phase_name = [i for i in names_in_big_groups_dict['phase']]
+    homog_name = [i for i in names_in_big_groups_dict['homogenization']]
+
+    values_in_phase = ['F','F_i','F_p','L_i','L_p','S','omega']
+
+    const_values_in_solver = ['C_minMaxAvg','C_volAvg','C_volAvgLastInc','F_aim','F_aimDot','F_aim_lastInc','P_aim']
+    changed_values_in_solver = ['F','F_lastInc']
     
-    const_group = ['HomogState']
-    
-    diff_values = ['F','Fi','Fp','Li','Lp','S','1_omega_plastic']
-    
-    diff_values_1 = ['F_lastInc','F']
-    
-    diff_groups = ['constituent']
-    
-    for i in const_values:
+    # creating solver group
+    for i in const_values_in_solver:
       f.create_dataset('/solver/' + i,data=np.array(hdf['/solver/' + i]))
+
+    xsize      = int(round(np.max(remeshed_coords[:,0])/(remeshed_coords[1,0] - remeshed_coords[0,0]))) #+ 1
+    ysize      = int(round(np.max(remeshed_coords[:,1])/(remeshed_coords[1,0] - remeshed_coords[0,0]))) #+ 1
+    zsize      = int(round(np.max(remeshed_coords[:,2])/(remeshed_coords[1,0] - remeshed_coords[0,0]))) #+ 1
+    totalsize  = int(xsize*ysize*zsize)
+
+    for i in changed_values_in_solver:
+      if i == 'F':
+        data2write = np.broadcast_to(np.eye(3).flatten(),tuple((zsize,ysize,xsize,9)))
+        f.create_dataset(f'solver/{i}',data = data2write)
+      if i == 'F_lastInc':
+        data2write = np.broadcast_to(np.eye(3), tuple((zsize,ysize,xsize,3,3)))
+        f.create_dataset(f'solver/{i}',data = data2write)
     
-    f.create_group('constituent')
-    f.create_group('materialpoint')
+    # creating homogenization group
+    for h in homog_name: 
+      f.create_group('homogenization/{}'.format(h))
 
-    for i in diff_values:
-      if i != '1_omega_plastic':
-        data_array = np.zeros((len(remeshed_coords),) + np.shape(hdf[i])[1:])
-        data_array[:] = np.array(hdf[i])[nbr_array]
-        f[i] = data_array  
-      else:
-        data_array = np.zeros((len(remeshed_coords),) + np.shape(hdf['/constituent/' + i])[1:])
-        data_array[:] = np.array(hdf['/constituent/' + i])[nbr_array]
-        f['/constituent/' + i] = data_array  
+    # creating phase group
+    for p in phase_name: 
+      f.create_group('phase/{}'.format(p))
 
-    for i in diff_values_1:
-        xsize      = int(round(np.max(remeshed_coords[:,0])/(remeshed_coords[1,0] - remeshed_coords[0,0]))) #+ 1
-        ysize      = int(round(np.max(remeshed_coords[:,1])/(remeshed_coords[1,0] - remeshed_coords[0,0]))) #+ 1
-        zsize      = int(round(np.max(remeshed_coords[:,2])/(remeshed_coords[1,0] - remeshed_coords[0,0]))) #+ 1
-        totalsize  = int(xsize*ysize*zsize)
-        data_array = np.zeros((totalsize,) + np.shape(hdf['/solver/' + i])[3:])
-        input_data = np.array(hdf['/solver/' + i]).reshape(((np.prod(np.shape(np.array(hdf['/solver/' + i]))[0:3]),)+np.shape(np.array(hdf['/solver/' + i]))[3:]))
-        data_array[:] = input_data[nbr_array]
-        data_array = data_array.reshape((zsize,ysize,xsize,) + np.shape(hdf['/solver/' + i])[3:])
-        f['/solver/' + i] = data_array
+      for i in values_in_phase:
+        data_array = np.zeros((totalsize,) + np.shape(hdf[f'/phase/{p}/' + i])[1:])
+        data_array[:] = np.array(hdf[f'/phase/{p}/' + i])[nbr_array]
+        f[f'/phase/{p}/' + i] = data_array
+
 
   def Initialize_Fp(self,restart_file_CA,casipt_output,remesh_file):
       """
